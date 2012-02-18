9d420d8c5a9755f7b422eb2ab4e8896aac5fad70
Chet Luther <cluther@zenoss.com>
2012-02-18 15:06:31 -0600

    Fix ZenPack.remove signature.

--- ZenPacks/example/Techniques/__init__.py
+++ ZenPacks/example/Techniques/__init__.py
@@ -27,7 +27,7 @@ class ZenPack(ZenPackBase):
     
     def remove(self, app, leaveObjects=False):
         self.removeMenuItems(app.zport.dmd)
-        ZenPackBase.remove(self, leaveObjects=leaveObjects)
+        ZenPackBase.remove(self, leaveObjects)
     
     def installMenuItems(self, dmd):
         self.removeMenuItems(dmd)
